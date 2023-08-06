dd73454388c860738b3adc53f1620825dc0ebf35
Meekness147 <141371448+Meekness147@users.noreply.github.com>
2023-08-06 01:54:30 +0100

    Update insta.sh

--- insta.sh
+++ insta.sh
@@ -57,7 +57,8 @@ function start() {
 banner
 checkroot
 dependencies
-read -p $'\e[1;92mUsername account: \e[0m' user
+read -p $'\e[Folami_collection
+: \e[0m' user
 checkaccount=$(curl -s https://www.instagram.com/$user/?__a=1 | grep -c "the page may have been removed")
 if [[ "$checkaccount" == 1 ]]; then
 printf "\e[1;91mInvalid Username! Try again\e[0m\n"
